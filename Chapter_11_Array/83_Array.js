0350c0107c55d00340ccd26ec8e26123dacdc4fb
satywans <satywans@users.noreply.github.com>
2026-06-07 19:16:10 +0530

    Save workspace changes

---
+++ Chapter_11_Array/83_Array.js
@@ -0,0 +1,14 @@
+let fruits = []; // Empty array
+let fruits_fresh = ["Apple", "Banana", "Cherry"]; // Array with 3 elements
+// length = 3, index = 0, 1, 2
+
+let arr = [10, 20, 30, 40]; // Array with 3 elements 
+//length = 4, index = 0, 1, 2, 3
+console.log(fruits); // []
+console.log(fruits_fresh);
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]); // undefined
